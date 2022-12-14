886a759395b7c9b3d5e943afc4b267f5d5d396e0
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-12-14 10:03:53 -0300

    feat: implement login generating jwt and type variables with interface models

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,13 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
 import { UnauthorizedException } from '@nestjs/common/exceptions';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './models/userPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './models/userToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userSerivce: UserService) {}
-  login() {
-    return 'Login service';
+  constructor(
+    private readonly userSerivce: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: User): UserToken {
+    //Transforma o user em JWT
+    const payLoad: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    const jwtToken = this.jwtService.sign(payLoad);
+    return {
+      access_token: jwtToken,
+    };
   }
+
   async validateUser(email: string, password: string) {
     const user = await this.userSerivce.findByEmail(email);
     if (user) {
