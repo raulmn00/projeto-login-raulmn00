85ea94f82f18973908a634580df502a60c130988
Raul Mariaci Neto <raul.mariaci@gmail.com>
2022-12-18 16:39:03 -0300

    fix: add configModule dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,7 +9,7 @@ import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
+  imports: [PrismaModule, UserModule, AuthModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [
     AppService,
